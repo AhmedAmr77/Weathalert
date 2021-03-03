b63025f574d2b8bd4ceac8cbe0a1930fd482d697
Ahmed Amr <ahmed.sharf.777@gmail.com>
2021-03-04 01:54:16 +0200

    getting location done

--- app/src/main/java/com/example/mvvm/home/view/HomeActivity.kt
+++ app/src/main/java/com/example/mvvm/home/view/HomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.Weathalert.home.view
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +27,8 @@ import com.example.Weathalert.datalayer.entity.WeatherData
 import com.example.Weathalert.favoritecities.view.FavoriteActivity
 import com.example.Weathalert.home.viewmodel.WeatherViewModel
 import com.example.Weathalert.settings.view.SettingsActivity
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnCompleteListener
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -30,6 +42,16 @@ class HomeActivity : AppCompatActivity() {
     private var hoursListAdapter = HoursAdapter(arrayListOf())
     private var daysListAdapter = DaysAdapter(arrayListOf())
 
+
+    lateinit var flpc: FusedLocationProviderClient
+    var longit = 0.0
+    var latit=0.0
+    var permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    var permissionID = 77
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -37,11 +59,29 @@ class HomeActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
 
-        observeViewModel(viewModel)
+        flpc = LocationServices.getFusedLocationProviderClient(getApplication() )
+//        setupNavigation()
+
+        getLastLoc()
+
+
         initUI()
+        observeViewModel(viewModel)
+
 
         favCitiesFabListener()
 
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionID) {  //maybe there are many permissions returned. Here we check for the location id that i defined before
+            if (permissions.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLoc()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -62,6 +102,106 @@ class HomeActivity : AppCompatActivity() {
         return true
     }
 
+
+    private fun checkForPermission(): Boolean {
+        return if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            false
+        } else
+            true
+    }
+    private fun locEnabled(): Boolean {
+        val locMngr = getSystemService(LOCATION_SERVICE) as LocationManager
+        return locMngr.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+    @SuppressLint("MissingPermission")
+    fun requestNewLoc() {
+        //Toast.makeText(MainActivity.this, "new REQUst", Toast.LENGTH_SHORT).show();
+        val locReq = LocationRequest() //LocationRequest.create()
+        locReq.setNumUpdates(1) //get only one udate then stop.
+        locReq.setInterval(0)
+        locReq.setFastestInterval(0)
+        locReq.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        flpc = LocationServices.getFusedLocationProviderClient(this)
+        flpc.requestLocationUpdates(locReq, locCallBack, Looper.getMainLooper())
+        //Toast.makeText(MainActivity.this, "after REQUst", Toast.LENGTH_SHORT).show();
+    }
+    private var locCallBack: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            //super.onLocationResult(locationResult);
+            val loc = locationResult.lastLocation
+            longit = loc.longitude
+            latit = loc.latitude
+        }
+    } // like listner
+    fun requestPremession() {                           //dialog for request permissions
+        ActivityCompat.requestPermissions(this, permissions, permissionID)
+    } // to know if user accept permission or not there is a callback method thats called automatically when user click on permissions button to tell what user answer.
+    private fun getLastLoc() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkForPermission()) {
+                Toast.makeText(this, "1111", Toast.LENGTH_SHORT).show()
+                if (locEnabled()) {
+                    Toast.makeText(this, "2222", Toast.LENGTH_SHORT).show()
+                    flpc.getLastLocation()
+                        .addOnCompleteListener(OnCompleteListener<Location?> { task ->
+                            val loc = task.getResult()
+                            if (loc != null) {
+                                Toast.makeText(
+                                    this,
+                                    loc.latitude.toString() + "\n" + loc.longitude.toString() + "",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else {
+                                requestNewLoc()
+                            }
+                        })
+                } else {
+                    val i = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(i)
+                }
+            } else {
+                Toast.makeText(this, "3333", Toast.LENGTH_SHORT).show()
+                requestPremession()
+            }
+        } else {
+            if (locEnabled()) {
+                flpc.getLastLocation().addOnCompleteListener(OnCompleteListener<Location?> { task ->
+                    val loc = task.result
+                    requestNewLoc()
+                    /*
+                                if (loc != null) {
+                                    requestNewLoc();
+                                    longit = loc.getLongitude()+"";
+                                    latit = loc.getLatitude()+"";
+                                    Toast.makeText(MainActivity.this, loc.getLatitude()+"\n"+loc.getLongitude(), Toast.LENGTH_LONG).show();
+                                } else {
+                                    //Toast.makeText(MainActivity.this, "new LOCAAAAAA", Toast.LENGTH_SHORT).show();
+                                    requestNewLoc();
+                                }*/
+                })
+            } else {
+                val i = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(i)
+            }
+        }
+    }
+
+    private fun setupNavigation() {
+        viewModel.showDialog.observe(this, Observer {
+            navigateToAddNewTask()
+        })
+
+//        viewModel.openTaskEvent.observe(this, Observer {
+//            openTaskDetails(it)
+//        })
+    }
+
+    private fun navigateToAddNewTask() {
+        val i = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(i)
+    }
+
     private fun initUI() {
         binding.hoursRecyclerView.apply {
             layoutManager =
@@ -79,7 +219,7 @@ class HomeActivity : AppCompatActivity() {
         viewModel.loadingLiveData.observe(this, { showLoading(it) })
         viewModel.errorLiveData.observe(this, { showError(it) })
         viewModel.fetchData().observe(this, Observer {
-            if (it != null){
+            if (it != null) {
                 updateUI(it)
             }
         })
--- app/src/main/java/com/example/mvvm/home/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/mvvm/home/viewmodel/WeatherViewModel.kt
@@ -1,13 +1,28 @@
 package com.example.Weathalert.home.viewmodel
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.Weathalert.datalayer.WeatherRepository
 import com.example.Weathalert.datalayer.entity.WeatherData
+import com.example.Weathalert.home.view.HomeActivity
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnCompleteListener
 import kotlinx.coroutines.*
 import retrofit2.Response
 import kotlin.math.log
@@ -17,11 +32,13 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
 //    val weatherLiveData = MutableLiveData<WeatherData>()
     val loadingLiveData = MutableLiveData<Boolean>()
     val errorLiveData = MutableLiveData<String>()
+    val showDialog = MutableLiveData<Unit>()
 
     private val weatherRepository = WeatherRepository(getApplication())
 
     private val lat = 61.9060
     private val lon = 92.4456
+
 
     fun fetchData(): LiveData<WeatherData> {
         loadingLiveData.postValue(false)
@@ -36,5 +53,84 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
 //            weatherLiveData.postValue(res)
 //        }
     }
-//
+/*
+    private fun checkForPermission(): Boolean {
+        return if (ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            false
+        } else
+            true
+    }
+    private fun locEnabled(): Boolean {        //HHHHHHBBBBBBBBDDDDDD
+        val locMngr = getApplication<Application>().getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
+        return locMngr.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+    @SuppressLint("MissingPermission")
+    fun requestNewLoc() {
+        //Toast.makeText(MainActivity.this, "new REQUst", Toast.LENGTH_SHORT).show();
+        val locReq = LocationRequest() //LocationRequest.create()
+        locReq.setNumUpdates(1) //get only one udate then stop.
+        locReq.setInterval(0)
+        locReq.setFastestInterval(0)
+        locReq.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        flpc = LocationServices.getFusedLocationProviderClient(getApplication())
+        flpc.requestLocationUpdates(locReq, locCallBack, Looper.getMainLooper())
+        //Toast.makeText(MainActivity.this, "after REQUst", Toast.LENGTH_SHORT).show();
+    }
+    private var locCallBack: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            //super.onLocationResult(locationResult);
+            val loc = locationResult.lastLocation
+            longit = loc.longitude
+            latit = loc.latitude
+        }
+    } // like listner
+    fun requestPremession() {                           //dialog for request permissions
+        ActivityCompat.requestPermissions(getApplication(), permissions, permissionID)
+    } // to know if user accept permission or not there is a callback method thats called automatically when user click on permissions button to tell what user answer.
+    fun getLastLoc() {
+        flpc = LocationServices.getFusedLocationProviderClient(getApplication() )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkForPermission()) {
+                Log.i("loc","checkForPermission")
+                if (locEnabled()) {
+                    Log.i("loc","locEnabled")
+                    flpc.getLastLocation()
+                        .addOnCompleteListener(OnCompleteListener<Location?> { task ->
+                            val loc = task.getResult()
+                            if (loc != null) {
+                                Log.i("loc","loc.latitude${loc.latitude}\nloc.longitude${loc.longitude}")
+                            } else {
+                                requestNewLoc()
+                            }
+                        })
+                } else {
+                    showDialog.value = Unit
+                }
+            } else {
+                Log.i("loc","loc NOT Enabled")
+                requestPremession()
+            }
+        } else {
+            if (locEnabled()) {
+                flpc.getLastLocation().addOnCompleteListener(OnCompleteListener<Location?> { task ->
+                    val loc = task.result
+                    requestNewLoc()
+                    /*
+                                if (loc != null) {
+                                    requestNewLoc();
+                                    longit = loc.getLongitude()+"";
+                                    latit = loc.getLatitude()+"";
+                                    Toast.makeText(MainActivity.this, loc.getLatitude()+"\n"+loc.getLongitude(), Toast.LENGTH_LONG).show();
+                                } else {
+                                    //Toast.makeText(MainActivity.this, "new LOCAAAAAA", Toast.LENGTH_SHORT).show();
+                                    requestNewLoc();
+                                }*/
+                })
+            } else {
+                showDialog.value = Unit
+            }
+        }
+    }
+*/
 }
