7a8389ff8e59cf59c7926bfbf7c77054b7060688
Ahmed Amr <ahmed.sharf.777@gmail.com>
2021-03-04 21:55:43 +0200

    use mapbox places

--- app/src/main/java/com/example/mvvm/favoritecities/view/FavoriteActivity.kt
+++ app/src/main/java/com/example/mvvm/favoritecities/view/FavoriteActivity.kt
@@ -2,22 +2,30 @@ package com.example.Weathalert.favoritecities.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.Weathalert.R
 import com.example.Weathalert.databinding.ActivityFavoriteBinding
-import com.example.Weathalert.datalayer.entity.Daily
-import com.example.Weathalert.datalayer.entity.Hourly
 import com.example.Weathalert.datalayer.entity.WeatherData
-import com.example.Weathalert.home.viewmodel.WeatherViewModel
 import com.example.mvvm.favoritecities.view.FavoriteAdapter
 import com.example.mvvm.favoritecities.viewmodel.FavoriteViewModel
+import com.example.mvvm.utils.Constants
+import com.mapbox.api.geocoding.v5.models.CarmenFeature
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.ui.PlaceAutocompleteFragment
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.ui.PlaceSelectionListener
 
 class FavoriteActivity : AppCompatActivity() {
 
     private lateinit var viewModel: FavoriteViewModel
     lateinit var binding: ActivityFavoriteBinding
     private var citisListAdapter = FavoriteAdapter(arrayListOf())
+
+    private var transaction : FragmentTransaction? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +37,42 @@ class FavoriteActivity : AppCompatActivity() {
         initUI()
         observeViewModel(viewModel)
 
+        favCitiesFabListener()
+
+    }
+
+    private fun favCitiesFabListener() {            ///WRITE IT IN VIEWMODEL
+        val fab: View = binding.addFavCityFab
+        fab.setOnClickListener {         //view ->
+            showAutoComplete()
+        }
+    }
+
+    private fun showAutoComplete() {
+        binding.searchFragmentContainer.visibility= View.VISIBLE
+        val autocompleteFragment = PlaceAutocompleteFragment.newInstance(Constants.MAPBOX_API_KEY)
+        transaction = supportFragmentManager?.beginTransaction()
+        transaction?.add(R.id.searchFragmentContainer, autocompleteFragment, Constants.AUTOCOMPLETE_FRAGMENT_TAG)
+        transaction?.commit()
+
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(carmenFeature: CarmenFeature) {
+                // TODO: Use the longitude and latitude
+                Toast.makeText(applicationContext,"latitude ${carmenFeature.center()?.latitude()} \n longitude ${carmenFeature.center()?.longitude()}"
+                    , Toast.LENGTH_LONG).show()
+                supportFragmentManager?.beginTransaction()?.remove(autocompleteFragment)?.commit()
+                binding.searchFragmentContainer.visibility= View.GONE
+
+                //add lat&long to DB and refresh RecyclerView
+
+            }
+
+            override fun onCancel() {
+                Log.i("places","cancel")
+                supportFragmentManager?.beginTransaction()?.remove(autocompleteFragment)?.commit()
+                binding.searchFragmentContainer.visibility= View.GONE
+            }
+        })
     }
 
     private fun initUI() {
@@ -54,4 +98,6 @@ class FavoriteActivity : AppCompatActivity() {
         citisListAdapter.updateHours(citiesList)
 //        daysListAdapter.updateDays(it.daily as List<Daily>)
     }
+
+
 }
--- app/src/main/java/com/example/mvvm/utils/Constants.kt
+++ app/src/main/java/com/example/mvvm/utils/Constants.kt
@@ -4,4 +4,8 @@ object Constants {
     const val LONGITUDE = "Longitude"
     const val LATITUDE = "Latitude"
     const val SHARED_PREF_LOCATION = "Location"
+
+    const val MAPBOX_API_KEY = "pk.eyJ1IjoiYWhtZC1hbXI3IiwiYSI6ImNrbHY5bHNlNzJudWwycW4xZWZ4ZnpwdmUifQ.i_uRDlSBTg9n27BabRXWKQ"
+    const val AUTOCOMPLETE_FRAGMENT_TAG = "autoCompleteFragment"
+
 }
