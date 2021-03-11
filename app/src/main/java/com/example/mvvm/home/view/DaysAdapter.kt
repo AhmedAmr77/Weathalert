62f1f1f8b36b2cc4c7121255e3ce592f60f574b7
Ahmed Amr <ahmed.sharf.777@gmail.com>
2021-03-11 20:54:23 +0200

    locale

--- app/src/main/java/com/example/mvvm/home/view/DaysAdapter.kt
+++ app/src/main/java/com/example/mvvm/home/view/DaysAdapter.kt
@@ -1,15 +1,12 @@
 package com.example.Weathalert.home.view
 
 import android.content.Context
-import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.Weathalert.R
 import com.example.Weathalert.databinding.DaysCellBinding
 import com.example.Weathalert.datalayer.entity.Daily
-import com.example.Weathalert.datalayer.entity.Hourly
-import java.io.Serializable
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -44,7 +41,7 @@ class DaysAdapter(var days: ArrayList<Daily>) : RecyclerView.Adapter<DaysAdapter
 
         fun bind(days: Daily) {
        //     binding.daysCellIcon.setImageResource(R.drawable.ic_baseline_cloud_24)  //(hours.weather[0].id)
-            binding.daysDayTVVal.text = (days.dt?.let { convert(it.toLong()) })?.get(2)
+            binding.daysDayTVVal.text = (days.dt?.let { convert(it.toLong()) })
             binding.daysGrtTempTVVal.text = days.temp?.max?.toInt().toString().plus("°")
             binding.daysSmlTempTVVal.text = days.temp?.min?.toInt().toString().plus("°")
             binding.daysDescTVVal.text = days.weather?.get(0)?.main
@@ -61,10 +58,10 @@ class DaysAdapter(var days: ArrayList<Daily>) : RecyclerView.Adapter<DaysAdapter
                     .format(systemTime).toString()
         }
         */
-        private fun convert(time: Long): ArrayList<String>? {
+        private fun convert(time: Long): String? {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                var ldt: LocalDateTime  = LocalDateTime.ofInstant(Instant.ofEpochSecond(time), ZoneId.systemDefault())
-                return arrayListOf(ldt.getMonth().toString(),ldt.getDayOfMonth().toString(), ldt.dayOfWeek.toString())
+                val ldt: LocalDateTime  = LocalDateTime.ofInstant(Instant.ofEpochSecond(time), ZoneId.systemDefault())
+                return getDay(ldt.dayOfWeek.toString())
             } else {
                 return null
             }
@@ -82,6 +79,20 @@ class DaysAdapter(var days: ArrayList<Daily>) : RecyclerView.Adapter<DaysAdapter
             return dt
 */
         }
+
+        private fun getDay(day: String): String {
+            return when (day){
+                "MONDAY" -> context.resources.getString(R.string.days_mon)
+                "TUESDAY" -> context.resources.getString(R.string.days_tue)
+                "WEDNESDAY" -> context.resources.getString(R.string.days_wed)
+                "THURSDAY" -> context.resources.getString(R.string.days_thu)
+                "FRIDAY" -> context.resources.getString(R.string.days_fri)
+                "SATURDAY" -> context.resources.getString(R.string.days_sat)
+                "SUNDAY" -> context.resources.getString(R.string.days_sun)
+                else -> "nullly"
+            }
+        }
+
     }
 }
 
--- app/src/main/java/com/example/mvvm/home/view/HoursAdapter.kt
+++ app/src/main/java/com/example/mvvm/home/view/HoursAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.Weathalert.home.view
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +20,7 @@ class HoursAdapter(var hours: ArrayList<Hourly>) : RecyclerView.Adapter<HoursAda
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : HoursVH {
         binding = HoursCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return HoursVH(binding)
+        return HoursVH(binding, parent.context)
     }
 
     override fun onBindViewHolder(holder: HoursVH, position: Int) {
@@ -35,23 +37,42 @@ class HoursAdapter(var hours: ArrayList<Hourly>) : RecyclerView.Adapter<HoursAda
         notifyDataSetChanged()
     }
 
-    class HoursVH(val binding: HoursCellBinding) : RecyclerView.ViewHolder(binding.root) {
+    class HoursVH(val binding: HoursCellBinding, val context: Context) : RecyclerView.ViewHolder(binding.root) {
         fun bind(hours: Hourly) {
-            binding.hoursCellHour.text = hours.dt?.let { convert(it.toLong()).toString() }
+            binding.hoursCellHour.text = hours.dt?.let { convert(it.toLong()) }
       //      binding.hoursCellIcon?.setImageResource(R.drawable.ic_baseline_favorite_24)  //(hours.weather[0].id)
             binding.hoursCellTemp.text = (hours.temp)?.toInt().toString().plus("°")
         }
 
-        private fun convert(time: Long): Serializable {
-            val dt = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                Instant.ofEpochSecond(time)
+        private fun convert(time: Long): String {
+            var dt = ""
+            Log.i("time", "convert start $time")
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                dt = Instant.ofEpochSecond(time)
                     .atZone(ZoneId.systemDefault())
                     .toLocalDateTime()
-                    .format(DateTimeFormatter.ofPattern("h a"))
+                    .format(DateTimeFormatter.ofPattern("k"))
+                Log.i("time", "convert dt $dt")
+                dt = amOrpm(dt)
             } else {
-                "NOT OREO"
+                dt = "NOT OREO"
             }
             return dt
+        }
+
+        private fun amOrpm(time: String): String{
+            Log.i("time", "amPm start $time")
+            var tm = time.toInt()
+            if ( tm > 11 && tm<24){
+                Log.i("time", "amPm >12 $tm")
+                if (tm != 12)
+                    tm -= 12
+                return tm.toString().plus(" ${context.resources.getString(R.string.home_hourly_pm)}")
+            }
+            Log.i("time", "else>12 $tm")
+            if (tm == 24)
+                tm -= 12
+            return "${(tm)} ${context.resources.getString(R.string.home_hourly_am)}"
         }
     }
 }
